4327b6773542399b8fd791f964e3ee8e78ab6007
kcmvp <kcheng.mvp@gmail.com>
2024-04-20 23:58:32 +0800

    delete unused files

--- name_rule.go
+++
@@ -1,8 +0,0 @@
-package archunit
-
-type NameRule interface {
-	NameShouldBeLowerCase() error
-	NameShouldBeUpperCase() error
-	NameShouldHavePrefix(prefix string) error
-	NameShouldHaveSuffix(suffix string) error
-}
